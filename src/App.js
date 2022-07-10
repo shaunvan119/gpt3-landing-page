6b1a0b9f985cebe8802b1b6668bd68daa5b5c401
shaunvan119 <shaunvan@live.com.au>
2022-07-10 16:23:51 +1000

    added container components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import Brand from "./components/brand/Brand";
 import Cta from "./components/cta/Cta";
 import Feature from "./components/feature/Feature";
 import Navbar from "./components/navbar/Navbar";
+import Blog from "./containers/blog/Blog";
+import Features from "./containers/features/Features";
+import Footer from "./containers/footer/Footer";
+import Header from "./containers/header/Header";
+import Possibility from "./containers/possibility/Possibility";
+import Whatgpt3 from "./containers/whatGPT3/Whatgpt3";
+
+
+
 
 function App() {
   return (
