2ca75943362c23992ff97d8063f659f3da81257b
shaunvan119 <shaunvan@live.com.au>
2022-07-12 16:20:42 +1000

    navbar div

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+
+      </div>
+    </div>
   )
 }
 
